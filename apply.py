d7a2f8aaaa3cdf5a9a3f71c0ddf3aa49d84b7157
Prernashekhawat3 <shekhawatprerna3@gmail.com>
2024-12-20 19:43:49 +0000

    default message functionality added

--- apply.py
+++ apply.py
@@ -13,15 +13,34 @@ app = Flask(__name__)
 # Email Configuration
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")  # Email address from .env
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-RESUME_PATH = "/mnt/d/prerna_resume/CV_Prerna_Shekhawat.pdf"  
+RESUME_PATH = "/mnt/d/prerna_resume/Resume_Prerna_Shekhawat.pdf"  
 
+# Default message and subject
+DEFAULT_SUBJECT = "Application for Python Developer - Prerna Shekhawat"
+DEFAULT_MESSAGE = """Dear [Recipient's Name],
+Hello HR,
+I hope this email finds you well.
+
+I am writing to express my keen interest in the Software Developer position. With a solid foundation in Python development and hands-on experience in building and deploying software solutions, I am eager to contribute to your team’s innovative projects.
+
+My recent experience includes working on a timesheet management platform using Flask, Python, and PostgreSQL, which has honed my skills in designing scalable systems and collaborating with cross-functional teams to deliver impactful results. I am confident that my technical expertise and problem-solving mindset align well with the requirements of this role.
+
+I have attached my resume for your review. I would welcome the opportunity to discuss how my skills and experiences can benefit your company goals. Please feel free to reach out to me at 8955495635 or via email at shekhawatprerna3@gmail.com to schedule an interview.
+
+Thank you for considering my application. I am excited about the possibility of joining your esteemed organization and contributing to its success.
+
+Best regards,
+Prerna Shekhawat
+LinkedIn: https://www.linkedin.com/in/prernashekhawat3/
+Phone: 8955495635
+"""
 
 @app.route("/send-email", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         hr_emails = request.form.get("hr_email").split(',')
-        subject = request.form.get("subject")
-        message = request.form.get("message")
+        subject = request.form.get("subject") and DEFAULT_SUBJECT
+        message = request.form.get("message") and DEFAULT_MESSAGE
 
         # Send email logic
         try:
@@ -43,7 +62,7 @@ def home():
             return redirect("/send-email")  # Redirect to the home page after sending emails
         except Exception as e:
             return f"An error occurred: {str(e)}"
-    return render_template("index.html")
+    return render_template("index.html", default_subject=DEFAULT_SUBJECT, default_message=DEFAULT_MESSAGE)
 
 if __name__ == "__main__":
     app.run(debug=True)
